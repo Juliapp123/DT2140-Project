d19ca31061981fcdb4f219b037ddac5d526fd5f9
Viktor Hamberg <vhamb@kth.se>
2025-12-08 17:20:29 +0100

    Combined state + lite ändringar i Gesture state

--- src/main/kotlin/furhatos/app/furhatlab/flow/CombinedState.kt
+++ src/main/kotlin/furhatos/app/furhatlab/flow/CombinedState.kt
@@ -15,22 +15,23 @@ import furhatos.nlu.common.Yes
 import furhatos.records.Location
 import furhatos.records.User
 import furhatos.util.Language
+import furhatos.flow.kotlin.Audio
 
-val g_model = OpenAIChatCompletionModel(serviceKey = "")
+val c_model = OpenAIChatCompletionModel(serviceKey = "")
 //glöm inte ta bort key innan vi pushar main!!
 
-val g_listOfPeople = listOf("Rihanna", "Drake", "Ed Sheeran", "Justin Bieber", "Taylor Swift",
+val c_listOfPeople = listOf("Rihanna", "Drake", "Ed Sheeran", "Justin Bieber", "Taylor Swift",
     "Christiano Ronaldo", "Donald Trump");
-var g_chosenPerson = "Rihanna"// g_listOfPeople.random();
+var c_chosenPerson = "Rihanna"// g_listOfPeople.random();
 
-val g_responseGenerator = ResponseGenerator(
+val c_responseGenerator = ResponseGenerator(
     /*systemPrompt = "You are a social robot who plays the game Guess Who. You are the one thinking of a person. The user will ask yes-or-no-questions about which person it is that you have selected. You only answer yes or no, unless it isn't a yes-or-no-quetion, in which case you explain that you can only answer yes-or-no-questions. Do not end the sentence with a question." +
             "The person you have chosen is " + g_chosenPerson,
     model = g_model */
     systemPrompt = """
         You are a social robot who plays the game Guess Who.
-        You are thinking of one person from this list: $g_listOfPeople.
-        The person you have chosen is: $g_chosenPerson.
+        You are thinking of one person from this list: $c_listOfPeople.
+        The person you have chosen is: $c_chosenPerson.
         
         The user will ask yes-or-no questions to guess who it is.
         
@@ -52,23 +53,23 @@ val g_responseGenerator = ResponseGenerator(
           NO|GOOD
           INVALID|MISC
     """.trimIndent(),
-    model = g_model
+    model = c_model
 )
 
-val GestureState = state {
+val CombinedState = state {
     var numberOfGuesses = 0;
     var numberOfQuestions = 0;
     var hasGuessedCorrectly = false;
 
     onEntry {
-        /*  val greeting = utterance {
-              +"Hi there!"
-              +"We are going to play Guess Who?"
-              +"Lets' start the game."
-          }
-          furhat.say(greeting) */
+        val greeting = utterance {
+            +"Hi there!"
+            +"We are going to play Guess Who?"
+            +"Lets' start the game."
+        }
+        furhat.say(greeting)
         //TEST avkommentera för testing
-        furhat.say(g_chosenPerson)
+        furhat.say(c_chosenPerson)
 
         furhat.gesture(Gestures.BigSmile(duration=4.0))
         furhat.gesture(Gestures.Nod(duration = 1.0))
@@ -79,27 +80,27 @@ val GestureState = state {
         furhat.listen()
     }
 
-    onResponse<g_EndGame> {
-        /* if(numberOfQuestions > 3) {
-             furhat.say("Thank you for playing, that was fun!")
-         } else {
-             furhat.say("Okay, see you another time.")
-         }
-         furhat.say("The person I was thinking of was " + g_chosenPerson) */
+    onResponse<c_EndGame> {
+        if(numberOfQuestions > 3) {
+            furhat.say("Thank you for playing, that was fun!")
+        } else {
+            furhat.say("Okay, see you another time.")
+        }
+        furhat.say("The person I was thinking of was " + c_chosenPerson)
         furhat.gesture(Gestures.Wink(duration=2.0))
         goto(Idle)
     }
 
-    onResponse<g_GuessPerson> {
+    onResponse<c_GuessPerson> {
         numberOfGuesses++
         numberOfQuestions++
 
-        if(g_chosenPerson == it.intent.g_person!!.value!!) {
+        if(c_chosenPerson == it.intent.c_person!!.value!!) {
             val finish = utterance {
                 +"Yes!"
                 +"Congratulations, you won"
                 +"It took you $numberOfGuesses guesses and $numberOfQuestions"
-                + "questions to find out I was thinking of $g_chosenPerson !"
+                + "questions to find out I was thinking of $c_chosenPerson !"
             }
             furhat.gesture(Gestures.BigSmile(duration = 3.0))
             furhat.gesture(Gestures.Nod(duration = 1.5))
@@ -108,46 +109,45 @@ val GestureState = state {
             numberOfGuesses = 0;
             numberOfQuestions = 0;
             hasGuessedCorrectly = false;
-            g_chosenPerson = "";
+            c_chosenPerson = "";
             goto(Idle)
         } else {
-            /* furhat.say("No") */
             furhat.gesture(Gestures.Shake(duration = 1.2))
+            furhat.say("No")
             reentry()
         }
     }
 
     onResponse {
-        /* val furhatResponse = g_responseGenerator.generate(this)
+        /* val furhatResponse = c_responseGenerator.generate(this)
         furhat.say(furhatResponse) */
-        val raw = g_responseGenerator.generate(this)  // e.g. "YES|STRONG"
+        val raw = c_responseGenerator.generate(this)  // e.g. "YES|STRONG"
         val parts = raw.split("|", limit = 2)
 
         val answerTag = parts.getOrNull(0)?.trim()
         val strengthTag = parts.getOrNull(1)?.trim()
 
         val answerType = when (answerTag) {
-            "YES" -> g_AnswerType.YES
-            "NO"  -> g_AnswerType.NO
-            else  -> g_AnswerType.INVALID
+            "YES" -> c_AnswerType.YES
+            "NO"  -> c_AnswerType.NO
+            else  -> c_AnswerType.INVALID
         }
 
         val strength = try {
-            strengthTag?.let { g_ResponseStrength.valueOf(it) } ?: g_ResponseStrength.MISC
+            strengthTag?.let { c_ResponseStrength.valueOf(it) } ?: c_ResponseStrength.MISC
         } catch (e: IllegalArgumentException) {
-            g_ResponseStrength.MISC
+            c_ResponseStrength.MISC
         }
 
-        // gesture-only feedback
-        // furhat.gesture(g_chooseGesture(answerType, strength))
-        // Changed to list based gestures for multiple at the same time
-        //furhat.gaze(Away)
-        //delay(2000)
-        //furhat.gaze(users.current)
-        // if (answerType == INVALID) // Lägga till om INVALID att den kollar bort under hela gesturen
-        new_g_chooseGestures(answerType, strength).forEach { gesture ->
+        // speech and gesture feedback
+        val prosody = utterance {
+            + c_chooseProsody(answerType, strength)
+        }
+
+        c_chooseGestures(answerType, strength).forEach { gesture ->
             furhat.gesture(gesture)
         }
+        furhat.say(prosody)
         numberOfQuestions++
         reentry()
     }
@@ -163,124 +163,123 @@ val GestureState = state {
 
 }
 
-class g_PeopleToChooseFrom : EnumEntity() {
-    override fun getEnum(lang: Language) = g_listOfPeople
+class c_PeopleToChooseFrom : EnumEntity() {
+    override fun getEnum(lang: Language) = c_listOfPeople
 }
 
-class g_GuessPerson(val g_person : g_PeopleToChooseFrom? = null) : Intent() {
-    override fun getExamples(lang: Language) = listOf("Is it $g_chosenPerson?", "Is the person $g_chosenPerson?", "$g_chosenPerson?")
+class c_GuessPerson(val c_person : g_PeopleToChooseFrom? = null) : Intent() {
+    override fun getExamples(lang: Language) = listOf("Is it $c_chosenPerson?", "Is the person $c_chosenPerson?", "$c_chosenPerson?")
 }
 
-class g_EndGame() : Intent(){
+class c_EndGame() : Intent(){
     override fun getExamples(lang: Language) = listOf("I do not want to play anymore", "End the game", "I give up", "Goodbye")
 }
 
-enum class g_AnswerType {
+enum class c_AnswerType {
     YES, NO, INVALID
 }
 
-enum class g_ResponseStrength {
+enum class c_ResponseStrength {
     GOOD, STRONG, HESITANT, MINIMAL, MISC
 }
 
+fun c_chooseProsody(answer: c_AnswerType, strength: c_ResponseStrength): Audio {
 
-fun g_chooseGesture(answer: g_AnswerType, strength: g_ResponseStrength): Gesture {
     return when (answer) {
-        // Finns parameter "strength" använda den för olika strengths + duration parameter eller olika gestures?
-        g_AnswerType.YES -> when (strength) {
-            g_ResponseStrength.STRONG   -> Gestures.BigSmile(duration = 2.0)     // strong yes
-            g_ResponseStrength.GOOD     -> Gestures.Smile(duration = 1.5)        // clear yes
-            g_ResponseStrength.HESITANT -> Gestures.Nod(duration = 0.8)          // soft nod
-            g_ResponseStrength.MINIMAL  -> Gestures.Nod(duration = 0.6)          // tiny nod
-            g_ResponseStrength.MISC     -> Gestures.Nod(duration = 0.6)
+        c_AnswerType.YES -> when (strength) {
+            c_ResponseStrength.STRONG ->  Audio("classpath:sound/yesyesyes.wav", "YES YES YES!!")
+            c_ResponseStrength.GOOD -> Audio("classpath:sound/yes.wav", "Yes")
+            c_ResponseStrength.HESITANT -> Audio("classpath:sound/hmmmyes.wav", "Hmmm... yes?")
+            c_ResponseStrength.MINIMAL -> Audio("classpath:sound/mhm.wav", "Mhm.")
+            c_ResponseStrength.MISC -> Audio("classpath:sound/yes.wav", "Yes")
         }
 
-        g_AnswerType.NO -> when (strength) {
-            g_ResponseStrength.STRONG   -> Gestures.Shake(duration = 1.5)        // strong no
-            g_ResponseStrength.GOOD     -> Gestures.Shake(duration = 1.0)        // clear no
-            g_ResponseStrength.HESITANT -> Gestures.BrowFrown(duration = 1.0)    // “no / not great”
-            g_ResponseStrength.MINIMAL  -> Gestures.Shake(duration = 0.6)        // small no
-            g_ResponseStrength.MISC     -> Gestures.BrowFrown(duration = 1.0)
+        c_AnswerType.NO -> when (strength) {
+            c_ResponseStrength.STRONG -> Audio("classpath:sound/absolutelynot.wav", "Absolutely NOT!")
+            c_ResponseStrength.GOOD -> Audio("classpath:sound/no.wav", "No")
+            c_ResponseStrength.HESITANT -> Audio("classpath:sound/hmmmno.wav", "Hmmm... no?")
+            c_ResponseStrength.MINIMAL -> Audio("classpath:sound/mm-mm(no).wav", "Mm-mm")
+            c_ResponseStrength.MISC -> Audio("classpath:sound/no.wav", "No")
         }
 
         // wrong type of question etc.
-        g_AnswerType.INVALID -> when (strength) {
-            g_ResponseStrength.STRONG,
-            g_ResponseStrength.GOOD,
-            g_ResponseStrength.MISC     -> Gestures.BrowFrown(duration = 1.0)    // “wrong question”
-            g_ResponseStrength.HESITANT -> Gestures.BrowFrown(duration = 0.8)
-            g_ResponseStrength.MINIMAL  -> Gestures.BrowFrown(duration = 0.8)
+        c_AnswerType.INVALID -> when (strength) {
+            c_ResponseStrength.STRONG -> Audio("classpath:sound/cantanswer.wav", "I can't really answer that.")
+            c_ResponseStrength.GOOD -> Audio("classpath:sound/cantanswer.wav", "I can't really answer that.")
+            c_ResponseStrength.MISC -> Audio("classpath:sound/notyesno.wav", "That's not a yes or no question")
+            c_ResponseStrength.HESITANT -> Audio("classpath:sound/dontknow.wav", "I don't know..")
+            c_ResponseStrength.MINIMAL -> Audio("classpath:sound/hmmm.wav", "Hmmmm...")
         }
     }
 }
 
-fun new_g_chooseGestures(answer: g_AnswerType, strength: g_ResponseStrength): List<Gesture> {
+fun c_chooseGestures(answer: c_AnswerType, strength: c_ResponseStrength): List<Gesture> {
     return when (answer) {
-        g_AnswerType.YES -> when (strength) {
+        c_AnswerType.YES -> when (strength) {
             // Strong yes
-            g_ResponseStrength.STRONG   -> listOf(
+            c_ResponseStrength.STRONG   -> listOf(
                 Gestures.BigSmile(duration = 2.0),
                 Gestures.Nod(duration = 1.2, strength = 1.5)
             )
             // Clear yes
-            g_ResponseStrength.GOOD     -> listOf(
+            c_ResponseStrength.GOOD     -> listOf(
                 Gestures.Smile(duration = 1.5),
                 Gestures.Nod(duration = 0.8, strength = 1.0)
             )
             // Uncertain yes
-            g_ResponseStrength.HESITANT -> listOf(
+            c_ResponseStrength.HESITANT -> listOf(
                 Gestures.Thoughtful(duration = 1.5),
                 Gestures.Nod(duration = 0.8, strength = 0.5)
             )
             // Weak yes
-            g_ResponseStrength.MINIMAL  -> listOf(
+            c_ResponseStrength.MINIMAL  -> listOf(
                 Gestures.Smile(duration = 1.2, strength = 0.6),
                 Gestures.Nod(duration = 0.6, strength = 0.5)
             )
-            g_ResponseStrength.MISC     -> listOf(
+            c_ResponseStrength.MISC     -> listOf(
                 Gestures.Thoughtful(duration = 1.0, strength = 1.5)
             )
         }
 
-        g_AnswerType.NO -> when (strength) {
+        c_AnswerType.NO -> when (strength) {
             // Strong no
-            g_ResponseStrength.STRONG   -> listOf(
+            c_ResponseStrength.STRONG   -> listOf(
                 Gestures.BrowFrown(duration = 2.0),
                 Gestures.Shake(duration = 1.2, strength = 1.5)
             )
             // Clear no
-            g_ResponseStrength.GOOD     -> listOf(
+            c_ResponseStrength.GOOD     -> listOf(
                 Gestures.BrowFrown(duration = 1.5),
                 Gestures.Shake(duration = 0.8, strength = 1.0)
             )
             // Uncertain no
-            g_ResponseStrength.HESITANT -> listOf(
+            c_ResponseStrength.HESITANT -> listOf(
                 Gestures.Thoughtful(duration = 1.5),
                 Gestures.Shake(duration = 0.8, strength = 0.5)
             )
             // Weak no
-            g_ResponseStrength.MINIMAL  -> listOf(
+            c_ResponseStrength.MINIMAL  -> listOf(
                 Gestures.BrowFrown(duration = 1.2, strength = 0.6),
                 Gestures.Shake(duration = 0.6, strength = 0.5)
             )
-            g_ResponseStrength.MISC     -> listOf(
+            c_ResponseStrength.MISC     -> listOf(
                 Gestures.Thoughtful(duration = 1.0, strength = 1.5)
             )
         }
 
         // wrong type of question etc.
-        g_AnswerType.INVALID -> when (strength) {
-            g_ResponseStrength.STRONG,
-            g_ResponseStrength.GOOD,
-            g_ResponseStrength.MISC     -> listOf(
+        c_AnswerType.INVALID -> when (strength) {
+            c_ResponseStrength.STRONG,
+            c_ResponseStrength.GOOD,
+            c_ResponseStrength.MISC     -> listOf(
                 Gestures.BrowFrown(duration = 2.0, strength = 0.6),
                 Gestures.Thoughtful(duration = 2.0, strength = 1.5)
             )
-            g_ResponseStrength.HESITANT -> listOf(
+            c_ResponseStrength.HESITANT -> listOf(
                 Gestures.BrowFrown(duration = 2.0, strength = 0.6),
                 Gestures.Thoughtful(duration = 2.0, strength = 1.5)
             )
-            g_ResponseStrength.MINIMAL  -> listOf(
+            c_ResponseStrength.MINIMAL  -> listOf(
                 Gestures.BrowFrown(duration = 2.0, strength = 0.6),
                 Gestures.Thoughtful(duration = 2.0, strength = 1.5)
             )
--- src/main/kotlin/furhatos/app/furhatlab/flow/StartInteraction.kt
+++ src/main/kotlin/furhatos/app/furhatlab/flow/StartInteraction.kt
@@ -2,6 +2,7 @@ package furhatos.app.furhatlab.flow
 
 import furhatos.app.furhatlab.flow.chat.ChatState
 import furhatos.app.furhatlab.flow.chat.GestureState
+import furhatos.app.furhatlab.flow.chat.CombinedState
 import furhatos.app.furhatlab.flow.fruitseller.FruitSellerGreeting
 import furhatos.flow.kotlin.State
 import furhatos.flow.kotlin.state
@@ -11,6 +12,7 @@ val StartInteraction: State = state {
         //goto(FruitSellerGreeting)
         //goto(GestureState)
         goto(ChatState)
+        //goto(CombinedState)
     }
 
 }
